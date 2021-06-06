14e736ebf9faa76b89ed1c8a1dce9c61385602e9
jaredait <jaredampudia@gmail.com>
2021-06-05 19:33:17 -0500

    Crear actividad para visualizar el historial de las citas

---
+++ Petit/app/src/main/java/com/enredados/petit/GUI/HistorialCitasActivity.java
@@ -0,0 +1,14 @@
+package com.enredados.petit.GUI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HistorialCitasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historial_citas);
+    }
+}
