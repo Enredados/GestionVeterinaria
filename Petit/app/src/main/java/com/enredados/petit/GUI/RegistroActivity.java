8ef42a4312b7b87b9a24c039227ca8d61ca90512
toka404 <58258902+toka404@users.noreply.github.com>
2021-06-05 21:41:00 -0500

    Guardado de seguridad

--- Petit/app/src/main/java/com/enredados/petit/GUI/RegistroActivity.java
+++ Petit/app/src/main/java/com/enredados/petit/GUI/RegistroActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.enredados.petit.R;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -35,6 +36,7 @@ public class RegistroActivity extends AppCompatActivity {
         setContentView(R.layout.activity_registro);
 
         //registro
+        Toolbar toolbar = findViewById(R.id.toolbar);
         registro();
     }
 
