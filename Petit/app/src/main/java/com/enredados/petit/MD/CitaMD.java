eef033dfac69cd954846fc43738c4365d552c761
jaredait <jaredampudia@gmail.com>
2021-06-05 22:18:32 -0500

    agregar constructor clase CitaMD

--- Petit/app/src/main/java/com/enredados/petit/MD/CitaMD.java
+++ Petit/app/src/main/java/com/enredados/petit/MD/CitaMD.java
@@ -1,4 +1,14 @@
 package com.enredados.petit.MD;
 
+import com.enredados.petit.DP.CitaDP;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class CitaMD {
+
+    private CitaDP citaDP;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private boolean validacion;
+
+    public CitaMD(CitaDP citaDP){ this.citaDP = citaDP; }
+
 }
